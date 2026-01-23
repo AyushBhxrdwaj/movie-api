77a56b1eb9092463428e4f76ab906db6282a2f4a
Ayush Bhardwaj <ayush.ds321@gmail.com>
2026-01-23 23:15:38 +0530

    validation fn

---
+++ src/middleware/validateRequest.js
@@ -0,0 +1,9 @@
+export const validateRequest = (schema)=>{
+    return (req,res,next)=>{
+        const result = schema.safeParse(req.body);
+        if(!result.success){
+            const errorMessages = res.error.errors.map((err)=>err.message)
+            return res.status(400).json({message:""})
+        }
+    }
+}
