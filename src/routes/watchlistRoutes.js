b7f7c8468ca6781040140fa61bd807c00d6180b1
Ayush Bhardwaj <ayush.ds321@gmail.com>
2026-01-24 22:52:06 +0530

    watchlist schema validator

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -5,12 +5,14 @@ import {
   updatefromWatchlist,
 } from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { watchlistSchema } from "../validators/watchlistValidators.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post("/", addToWatchList);
+router.post("/",validateRequest(watchlistSchema),addToWatchList);
 router.put("/:id",updatefromWatchlist);
 router.delete("/:id", removefromWatchlist);
 export default router;
